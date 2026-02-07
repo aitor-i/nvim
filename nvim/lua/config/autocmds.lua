93deff9ac1e819ae95bb1c46fe061c2f0cdefc85
Aitor <54040099+aitor-i@users.noreply.github.com>
2026-02-07 11:05:36 +0100

    Guard LSP format on save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -16,7 +16,18 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function()
-    vim.lsp.buf.format({ async = false })
+    local clients = vim.lsp.get_clients({ bufnr = 0 })
+    local has_formatter = false
+    for _, client in ipairs(clients) do
+      if client.supports_method("textDocument/formatting") then
+        has_formatter = true
+        break
+      end
+    end
+
+    if has_formatter then
+      vim.lsp.buf.format({ async = false, timeout_ms = 2000 })
+    end
   end,
 })
 
