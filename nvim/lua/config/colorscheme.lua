8fce2475ce4b16dc13edb4f044ca09f5cd705ef0
Aitor <contact@aitoribarra.com>
2026-05-14 19:02:00 +0200

    feat: reads color schema from omarchy

--- nvim/lua/config/colorscheme.lua
+++ nvim/lua/config/colorscheme.lua
@@ -1,34 +1,36 @@
 local available_colorschemes = {
-  "lunar",
-  "tokyonight",
-  "catppuccin",
-  "kanagawa",
-  "nightfox",
-  "gruvbox",
+	"tokyonight",
+	"lunar",
+	"catppuccin",
+	"kanagawa",
+	"nightfox",
+	"gruvbox",
+	"everforest",
+	"aether",
 }
 
 local function apply_colorscheme(name)
-  local target = name and name ~= "" and name or vim.g.colorscheme
-  if not target or target == "" then
-    target = "lunar"
-  end
+	local target = name and name ~= "" and name or vim.g.colorscheme
+	if not target or target == "" then
+		target = vim.g.omarchy_colorscheme or "everforest"
+	end
 
-  local ok = pcall(vim.cmd.colorscheme, target)
-  if not ok then
-    vim.notify("Colorscheme '" .. target .. "' not found", vim.log.levels.WARN)
-    return
-  end
+	local ok = pcall(vim.cmd.colorscheme, target)
+	if not ok then
+		vim.notify("Colorscheme '" .. target .. "' not found", vim.log.levels.WARN)
+		return
+	end
 
-  vim.g.colorscheme = target
+	vim.g.colorscheme = target
 end
 
 vim.api.nvim_create_user_command("ColorScheme", function(opts)
-  apply_colorscheme(opts.args)
+	apply_colorscheme(opts.args)
 end, {
-  nargs = "?",
-  complete = function()
-    return available_colorschemes
-  end,
+	nargs = "?",
+	complete = function()
+		return available_colorschemes
+	end,
 })
 
 apply_colorscheme(vim.g.colorscheme)
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,190 +1,231 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  { "LunarVim/lunar.nvim", priority = 1000 },
-  { "folke/tokyonight.nvim", priority = 1000 },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "rebelot/kanagawa.nvim", priority = 1000 },
-  { "EdenEast/nightfox.nvim", priority = 1000 },
-  { "ellisonleao/gruvbox.nvim", priority = 1000 },
-  { "nvim-lua/plenary.nvim" },
-  { "nvim-tree/nvim-web-devicons" },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({})
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local ok, configs = pcall(require, "nvim-treesitter.configs")
-      if not ok then
-        vim.notify("nvim-treesitter.configs not found; skipping setup", vim.log.levels.WARN)
-        return
-      end
-      configs.setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "javascript",
-          "json",
-          "lua",
-          "python",
-          "typescript",
-          "tsx",
-          "css",
-          "rust",
-          "java",
-          "yaml",
-        },
-        highlight = { enable = true },
-        autotag = { enable = true },
-        indent = { enable = true, disable = { "typescript", "tsx" } },
-      })
-    end,
-  },
-  { "windwp/nvim-ts-autotag", dependencies = { "nvim-treesitter/nvim-treesitter" } },
-  {
-    "ThePrimeagen/harpoon",
-    config = function()
-      require("harpoon").setup({})
-    end,
-  },
-  { "mbbill/undotree" },
-  { "williamboman/mason.nvim", config = function() require("mason").setup({}) end },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-  },
-  { "stevearc/conform.nvim" },
-  { "mfussenegger/nvim-lint" },
-  { "jparise/vim-graphql" },
-  { "kdheepak/lazygit.nvim" },
-  {
-    "onsails/lspkind-nvim",
-    config = function()
-      require("lspkind").init({
-        symbol_map = { Supermaven = "" },
-      })
-      vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
-    end,
-  },
-  {
-    "supermaven-inc/supermaven-nvim",
-    config = function()
-      require("supermaven-nvim").setup({
-        keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-j>",
-        },
-        ignore_filetypes = { "cpp" },
-        color = { suggestion_color = "#A9A9A9", cterm = 244 },
-        log_level = "info",
-        disable_inline_completion = false,
-        disable_keymaps = true,
-        condition = function()
-          return false
-        end,
-      })
-    end,
-  },
-  {
-    "f-person/git-blame.nvim",
-    config = function()
-      vim.g.gitblame_enabled = 0
-    end,
-  },
-  { "editorconfig/editorconfig-vim" },
-  { "neovim/nvim-lspconfig" },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "saadparwaiz1/cmp_luasnip",
-      "L3MON4D3/LuaSnip",
-      "windwp/nvim-autopairs",
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local function load_omarchy_theme_specs()
+	local path = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+	if vim.fn.filereadable(path) ~= 1 then
+		return {}
+	end
 
-      require("nvim-autopairs").setup({
-        check_ts = true,
-      })
+	local ok, specs = pcall(dofile, path)
+	if not ok or type(specs) ~= "table" then
+		vim.notify("Could not load Omarchy Neovim theme: " .. tostring(specs), vim.log.levels.WARN)
+		return {}
+	end
 
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	local theme_specs = {}
+	for _, spec in ipairs(specs) do
+		if type(spec) == "table" and spec[1] == "LazyVim/LazyVim" then
+			-- Omarchy theme files include this only to tell LazyVim which colorscheme to use.
+			-- This config is plain Neovim + lazy.nvim, so keep the value and skip the plugin.
+			vim.g.omarchy_colorscheme = spec.opts and spec.opts.colorscheme or vim.g.omarchy_colorscheme
+		elseif type(spec) == "table" then
+			table.insert(theme_specs, spec)
+		end
+	end
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered({
-            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-          }),
-          documentation = cmp.config.window.bordered({
-            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-          }),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-@>"] = cmp.mapping.complete(),
-          ["<C-2>"] = cmp.mapping.complete(),
-          ["<CR>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.confirm({ select = true })
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "supermaven" },
-        }),
-      })
-    end,
-  },
-  { "folke/which-key.nvim", config = function() require("which-key").setup({}) end },
-})
+	return theme_specs
+end
+
+local plugins = {
+	{ "LunarVim/lunar.nvim", priority = 1000 },
+	{ "neanias/everforest-nvim", priority = 1000 },
+	{ "folke/tokyonight.nvim", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "rebelot/kanagawa.nvim", priority = 1000 },
+	{ "EdenEast/nightfox.nvim", priority = 1000 },
+	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
+	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-tree/nvim-web-devicons" },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local ok, configs = pcall(require, "nvim-treesitter.configs")
+			if not ok then
+				vim.notify("nvim-treesitter.configs not found; skipping setup", vim.log.levels.WARN)
+				return
+			end
+			configs.setup({
+				ensure_installed = {
+					"bash",
+					"c",
+					"javascript",
+					"json",
+					"lua",
+					"python",
+					"typescript",
+					"tsx",
+					"css",
+					"rust",
+					"java",
+					"yaml",
+				},
+				highlight = { enable = true },
+				autotag = { enable = true },
+				indent = { enable = true, disable = { "typescript", "tsx" } },
+			})
+		end,
+	},
+	{ "windwp/nvim-ts-autotag", dependencies = { "nvim-treesitter/nvim-treesitter" } },
+	{
+		"ThePrimeagen/harpoon",
+		config = function()
+			require("harpoon").setup({})
+		end,
+	},
+	{ "mbbill/undotree" },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+	},
+	{ "stevearc/conform.nvim" },
+	{ "mfussenegger/nvim-lint" },
+	{ "jparise/vim-graphql" },
+	{ "kdheepak/lazygit.nvim" },
+	{
+		"onsails/lspkind-nvim",
+		config = function()
+			require("lspkind").init({
+				symbol_map = { Supermaven = "" },
+			})
+			vim.api.nvim_set_hl(0, "CmpItemKindSupermaven", { fg = "#6CC644" })
+		end,
+	},
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<Tab>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-j>",
+				},
+				ignore_filetypes = { "cpp" },
+				color = { suggestion_color = "#A9A9A9", cterm = 244 },
+				log_level = "info",
+				disable_inline_completion = false,
+				disable_keymaps = true,
+				condition = function()
+					return false
+				end,
+			})
+		end,
+	},
+	{
+		"f-person/git-blame.nvim",
+		config = function()
+			vim.g.gitblame_enabled = 0
+		end,
+	},
+	{ "editorconfig/editorconfig-vim" },
+	{ "neovim/nvim-lspconfig" },
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+			"windwp/nvim-autopairs",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+			require("nvim-autopairs").setup({
+				check_ts = true,
+			})
+
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered({
+						winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+					}),
+					documentation = cmp.config.window.bordered({
+						winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+					}),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-@>"] = cmp.mapping.complete(),
+					["<C-2>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.confirm({ select = true })
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "supermaven" },
+				}),
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({})
+		end,
+	},
+}
+
+vim.list_extend(plugins, load_omarchy_theme_specs())
+
+require("lazy").setup(plugins)
